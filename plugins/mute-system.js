7397936e3a6d3c5819df5dbfbfc1c5260b8f7bd6
friendLucifer <zenentoji471@gmail.com>
2026-06-05 02:54:02 +0300

    Update mute-system.js

--- plugins/mute-system.js
+++ plugins/mute-system.js
@@ -1,29 +1,9 @@
-const handler = async (m, { conn }) => {
+const handler = async (m) => {
 
-  if (!m.isGroup) return
-
-  global.db = global.db || {}
-  global.db.data = global.db.data || {}
-  global.db.data.muted = global.db.data.muted || {}
-
-  console.log("تم تشغيل نظام الكتم")
-  console.log("Sender:", m.sender)
-  console.log("Muted List:", global.db.data.muted)
-
-  let data = global.db.data.muted[m.sender]
-
-  if (!data) return
-
-  if (Date.now() > data.time) {
-    delete global.db.data.muted[m.sender]
-    return
+  if (m.text === "اختبار") {
+    await m.reply("نظام الكتم يعمل")
   }
 
-  console.log("تم العثور على عضو مكتوم")
-
-  await conn.sendMessage(m.chat, {
-    delete: m.key
-  })
 }
 
 handler.all = true
