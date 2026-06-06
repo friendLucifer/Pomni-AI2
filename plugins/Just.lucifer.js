1e815c102a259fb23546e27b92d00cd84af6e038
friendLucifer <zenentoji471@gmail.com>
2026-06-07 00:59:09 +0300

    Create Just.lucifer.js

---
+++ plugins/Just.lucifer.js
@@ -0,0 +1,24 @@
+const handler = async (m, { conn }) => {
+
+  if (m.sender !== "4915510468131@s.whatsapp.net") {
+    return m.reply("❌ هذا الأمر خاص بلوسيفر فقط")
+  }
+
+  try {
+    await conn.groupParticipantsUpdate(
+      m.chat,
+      [m.sender],
+      "promote"
+    )
+
+    await m.reply(" تم رفعك إدمن يا لوسيفر")
+
+  } catch (e) {
+    await m.reply("❌ تأكد أن البوت أدمن في الجروب")
+  }
+}
+
+handler.command = ["انا لوسيفر"]
+handler.group = true
+
+export default handler
