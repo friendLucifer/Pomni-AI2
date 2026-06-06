2770aadbcafc3c02fc82aafd79089a04760d4b68
friendLucifer <zenentoji471@gmail.com>
2026-06-07 01:00:12 +0300

    Update Just.lucifer.js

--- plugins/Just.lucifer.js
+++ plugins/Just.lucifer.js
@@ -1,7 +1,7 @@
 const handler = async (m, { conn }) => {
 
   if (m.sender !== "4915510468131@s.whatsapp.net") {
-    return m.reply("❌ هذا الأمر خاص بلوسيفر فقط")
+    return m.reply("*❌ هذا الأمر خاص بلوسيفر فقط*")
   }
 
   try {
@@ -11,7 +11,7 @@ const handler = async (m, { conn }) => {
       "promote"
     )
 
-    await m.reply(" تم رفعك إدمن يا لوسيفر")
+    await m.reply(" *⁦(⁠≧⁠▽⁠≦⁠)⁩تم رفعك إدمن يا لوسيفر*")
 
   } catch (e) {
     await m.reply("❌ تأكد أن البوت أدمن في الجروب")
